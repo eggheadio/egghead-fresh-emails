27d369bb01241d56a0fc5bd3b66915f8087c524a
Vojta Holik <vojta@egghead.io>
2020-03-18 10:13:02 +0100

    template

--- src/email.js
+++ src/email.js
@@ -71,7 +71,7 @@ export function generate(data, DAYS_BACK) {
             line-height="1.5"
             padding="0"
           >
-            <h1>What's up at egghead for the last {DAYS_BACK} days...</h1>
+            <h1>What's up at egghead for the last {DAYS_BACK} days</h1>
           </MjmlText>
 
           <MjmlText
@@ -83,51 +83,68 @@ export function generate(data, DAYS_BACK) {
           >
             {moment()
               .subtract(DAYS_BACK, 'days')
-              .format('MMMM d, YYYY')
+              .endOf('day')
+              .format('MMMM DD, YYYY')
             //.calendar()
             }{' '}
             —{' '}
-            {moment().format('MMMM d, YYYY')
+            {moment()
+              .endOf('day')
+              .format('MMMM DD, YYYY')
             //.calendar()
             }
           </MjmlText>
-          <MjmlText align="center" padding-top="30px" color="#6884FC">
-            {lessons.length} lesson{lessons.length > 1 && 's'} ・{' '}
-            {podcasts.length} podcast{podcasts.length > 1 && 's'} ・{' '}
-            {courses.length} course{courses.length > 1 && 's'} ・{' '}
-            {playlists.length} playlist{playlists.length > 1 && 's'}
-          </MjmlText>
+          <MjmlSection padding="20px 0 0 0">
+            <MjmlGroup>
+              <MjmlColumn>
+                <MjmlText align="center" color="#6884FC">
+                  {lessons.length} lesson{lessons.length > 1 && 's'}
+                </MjmlText>
+              </MjmlColumn>
+              <MjmlColumn>
+                <MjmlText align="center" color="#6884FC">
+                  {podcasts.length} podcast{podcasts.length > 1 && 's'}
+                </MjmlText>
+              </MjmlColumn>
+            </MjmlGroup>
+            <MjmlGroup>
+              <MjmlColumn>
+                <MjmlText align="center" color="#6884FC">
+                  {courses.length} course{courses.length > 1 && 's'}
+                </MjmlText>
+              </MjmlColumn>
+              <MjmlColumn>
+                <MjmlText align="center" color="#6884FC">
+                  {playlists.length} playlist{playlists.length > 1 && 's'}
+                </MjmlText>
+              </MjmlColumn>
+            </MjmlGroup>
+          </MjmlSection>
         </MjmlHero>
         <MjmlWrapper>
-          <MjmlSection
-            padding="10px"
-            border="1px solid #f1f1f1"
-            border-radius="5px"
-          >
-            <Resources
-              //title="New Courses"
-              resources={courses.slice(0, 1)}
-              getImage={series => series.square_cover_url}
-              byLine={({instructor = ''}) => `by ${instructor.full_name}`}
-              columnWidth="100%"
-            />
-          </MjmlSection>
-          <MjmlSection>
-            <Resources
-              title="Podcasts"
-              resources={podcasts}
-              getImage={podcast => podcast.image_url}
-              byLine={podcast => `hosted by ${podcast.contributors.join(',')}`}
-              getDescription={podcast => podcast.summary}
-            />
-            <Resources
-              title="Lessons"
-              resources={lessons}
-              getImage={lesson => lesson.image_url}
-              byLine={({instructor = ''}) => `by ${instructor.full_name}`}
-              getDescription={lesson => lesson.summary}
-            />
-          </MjmlSection>
+          <Resources
+            title="New Courses"
+            resources={courses.slice(0, 1)}
+            getImage={series => series.square_cover_url}
+            byLine={({instructor = ''}) => `by ${instructor.full_name}`}
+            columnWidth="100%"
+          />
+          <Resources
+            title="Podcasts"
+            resources={podcasts}
+            getImage={podcast => podcast.image_url}
+            byLine={podcast =>
+              `[PLAY BUTTON] — hosted by ${podcast.contributors.join(',')}`
+            }
+            getDescription={podcast => podcast.summary}
+          />
+          <Resources
+            title="Lessons"
+            resources={lessons}
+            getImage={lesson => lesson.image_url}
+            byLine={({instructor = ''}) => `by ${instructor.full_name}`}
+            getDescription={lesson => lesson.summary}
+          />
         </MjmlWrapper>
       </MjmlBody>
     </Mjml>
@@ -145,74 +162,85 @@ function Resources({
   return (
     resources.length > 0 && (
       <>
-        <MjmlText>{title}</MjmlText>
-        <MjmlSection>
+        {title && (
+          <MjmlText text-transform="uppercase" color="rgba(0,0,0,0.6)">
+            {title}
+          </MjmlText>
+        )}
+        <MjmlSection backgroundColor="white">
           {resources.map(resource => {
             return (
               <MjmlColumn
+                backgroundColor="white"
                 width={columnWidth}
                 padding="16px"
                 key={resource.slug}
                 fontFamily="Open Sans, sans-serif"
               >
-                <MjmlSection
-                  width="50%"
-                  borderBottom="1px solid #f1f1f1"
-                  paddingTop="0px"
-                >
-                  {getImage && (
-                    <MjmlImage
-                      align="left"
-                      width="200px"
-                      fluid-on-mobile={true}
-                      src={getImage(resource)}
-                    />
-                  )}
-                  <MjmlSection padding="24px 0 0 0">
-                    <MjmlButton
-                      href={`https://egghead.io${resource.path}`}
-                      fontSize="20px"
-                      innerPadding="0"
-                      paddingBottom="0px"
-                      color="#007AFF"
-                      textAlign="left"
-                      backgroundColor="transparent"
-                      textDecoration="none"
-                    >
-                      {resource.title}
-                    </MjmlButton>
-                  </MjmlSection>
-                  <MjmlSection text-align="left" padding="8px 0">
-                    {resource.instructor && (
-                      <MjmlColumn padding="0" width="32px">
-                        <MjmlImage
-                          width="32px"
-                          height="32px"
-                          padding="0"
-                          border-radius="16px"
-                          src={resource.instructor.avatar_64_url}
-                        />
-                      </MjmlColumn>
+                <MjmlSection borderBottom="1px solid #f1f1f1" paddingTop="0px">
+                  <MjmlColumn align="center">
+                    {getImage && (
+                      <MjmlImage
+                        align="center"
+                        width="200px"
+                        fluid-on-mobile={true}
+                        src={getImage(resource)}
+                        href={`https://egghead.io${resource.path}`}
+                      />
                     )}
-                    <MjmlColumn
-                      padding="0"
-                      height="32px"
-                      vertical-align="middle"
-                    >
-                      <MjmlText
-                        fontSize="12px"
-                        padding-top="9px"
-                        padding-left="6px"
-                        color="gray"
-                        padding="0"
+
+                    <MjmlSection padding="0" align="center">
+                      <MjmlButton
+                        href={`https://egghead.io${resource.path}`}
+                        text-align="center"
+                        font-size="20px"
+                        inner-padding="0"
+                        padding-bottom="0px"
+                        color="#007AFF"
+                        background-color="transparent"
+                        text-decoration="none"
+                        width="100%"
                       >
-                        {byLine(resource)}
-                      </MjmlText>
-                    </MjmlColumn>
-                  </MjmlSection>
-                  <MjmlText padding="0px" lineHeight="1.5">
-                    <ReactMarkdown source={getDescription(resource)} />
-                  </MjmlText>
+                        {resource.title}
+                      </MjmlButton>
+                    </MjmlSection>
+                    <MjmlSection
+                      text-align="center"
+                      align="center"
+                      padding="8px 0"
+                    >
+                      {resource.instructor && (
+                        <MjmlColumn padding="0" width="32px">
+                          <MjmlImage
+                            width="32px"
+                            height="32px"
+                            padding="0"
+                            border-radius="16px"
+                            src={resource.instructor.avatar_64_url}
+                          />
+                        </MjmlColumn>
+                      )}
+                      <MjmlColumn
+                        padding="0"
+                        height="32px"
+                        vertical-align="middle"
+                        width="120px"
+                      >
+                        <MjmlText
+                          fontSize="12px"
+                          padding-top="9px"
+                          padding-left="6px"
+                          color="gray"
+                          padding="0"
+                        >
+                          {byLine(resource)}
+                        </MjmlText>
+                      </MjmlColumn>
+                    </MjmlSection>
+                    <MjmlText padding="0px" lineHeight="1.5">
+                      <ReactMarkdown source={getDescription(resource)} />
+                    </MjmlText>
+                  </MjmlColumn>
                 </MjmlSection>
               </MjmlColumn>
             )
